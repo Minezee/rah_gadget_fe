d841e643b16539be3e1c6d61cff3065cba0a988b
NofathZ <nofath86@gmail.com>
2022-02-09 20:55:08 +0700

    add className opt on typography

--- components/Typography/Typography.tsx
+++ components/Typography/Typography.tsx
@@ -16,59 +16,59 @@ import {
 } from "./typography.elements";
 
 const H1 = (props: any) => {
-  return <H1Style>{props.children}</H1Style>;
+  return <H1Style className={props.className}>{props.children}</H1Style>;
 };
 
 const H2 = (props: any) => {
-  return <H2Style>{props.children}</H2Style>;
+  return <H2Style className={props.className}>{props.children}</H2Style>;
 };
 
 const H3 = (props: any) => {
-  return <H3Style>{props.children}</H3Style>;
+  return <H3Style className={props.className}>{props.children}</H3Style>;
 };
 
 const SH1 = (props: any) => {
-  return <SH1Style>{props.children}</SH1Style>;
+  return <SH1Style className={props.className}>{props.children}</SH1Style>;
 };
 
 const SH2 = (props: any) => {
-  return <SH2Style>{props.children}</SH2Style>;
+  return <SH2Style className={props.className}>{props.children}</SH2Style>;
 };
 
 const SH3 = (props: any) => {
-  return <SH3Style>{props.children}</SH3Style>;
+  return <SH3Style className={props.className}>{props.children}</SH3Style>;
 };
 
 const Lead1 = (props: any) => {
-  return <Lead1Style>{props.children}</Lead1Style>;
+  return <Lead1Style className={props.className}>{props.children}</Lead1Style>;
 };
 
 const Lead2 = (props: any) => {
-  return <Lead2Style>{props.children}</Lead2Style>;
+  return <Lead2Style className={props.className}>{props.children}</Lead2Style>;
 };
 
 const Lead3 = (props: any) => {
-  return <Lead3Style>{props.children}</Lead3Style>;
+  return <Lead3Style className={props.className}>{props.children}</Lead3Style>;
 };
 
 const Body1 = (props: any) => {
-  return <Body1Style>{props.children}</Body1Style>;
+  return <Body1Style className={props.className}>{props.children}</Body1Style>;
 };
 
 const Body2 = (props: any) => {
-  return <Body2Style>{props.children}</Body2Style>;
+  return <Body2Style className={props.className}>{props.children}</Body2Style>;
 };
 
 const Label1 = (props: any) => {
-  return <Label1Style>{props.children}</Label1Style>;
+  return <Label1Style className={props.className}>{props.children}</Label1Style>;
 };
 
 const Label2 = (props: any) => {
-  return <Label2Style>{props.children}</Label2Style>;
+  return <Label2Style className={props.className}>{props.children}</Label2Style>;
 };
 
 const Label3 = (props: any) => {
-  return <Label3Style>{props.children}</Label3Style>;
+  return <Label3Style className={props.className}>{props.children}</Label3Style>;
 };
 
 export {
